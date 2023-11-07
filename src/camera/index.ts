c1793cd49a258f61c82fc1764ef41c7eaa766206
Glen Lee <glenleecareer@gmail.com>
2023-11-07 13:22:40 +0000

    add extra types, add orthographic camera option

--- src/camera/index.ts
+++ src/camera/index.ts
@@ -1,12 +1,23 @@
 import * as THREE from "three";
 
+let height = window.innerHeight;
+let width = window.innerWidth;
 export function createCamera(): THREE.PerspectiveCamera {
-  const camera = new THREE.PerspectiveCamera(
-    70,
-    window.innerWidth / window.innerHeight,
-    0.01,
-    1000
-  );
+  const camera = new THREE.PerspectiveCamera(70, width / height, 0.01, 1000);
   camera.position.z = 1;
   return camera;
 }
+
+// export function createCamera(): THREE.OrthographicCamera {
+//   let frustumSize = height;
+//   let aspect = width / height;
+//   let camera = new THREE.OrthographicCamera(
+//     (frustumSize * aspect) / -2,
+//     (frustumSize * aspect) / 2,
+//     frustumSize / 2,
+//     frustumSize / -2,
+//     -1000,
+//     1000
+//   );
+//   return camera;
+// }
--- types/index.d..ts
+++ types/index.d..ts
@@ -2,3 +2,7 @@ declare module "*.glsl" {
   const value: string;
   export default value;
 }
+
+declare module "*.jpg";
+declare module "*.jpeg";
+declare module "*.png";
